f003f0d986b0face283988e7288075be361d08c2
DionatanLeao <dionatan.ti@outlook.com>
2021-10-24 18:02:14 -0300

    Service

---
+++ crud/src/main/java/com/practice/crud/GenericClassService.java
@@ -0,0 +1,49 @@
+package com.practice.crud;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.practice.crud.entities.GenericClass;
+import com.practice.crud.repositories.GenericClassRepository;
+
+
+@Service
+public class GenericClassService {
+
+	@Autowired
+	private GenericClassRepository repo;
+
+	public List<GenericClass> findAll() {
+		return repo.findAll();
+	}
+
+	public Optional<GenericClass> findById(Integer id) {
+		return repo.findById(id);
+	}
+	
+	public GenericClass save(GenericClass crud) {
+		return repo.save(crud);
+	}
+	
+	public GenericClass update(GenericClass crudUpdate, Integer id) throws Exception {
+		GenericClass genericClass = repo.findById(id).orElseThrow(() -> new Exception("genericClass not found: " + id));
+		genericClass.setId(crudUpdate.getId());
+		genericClass.setName(crudUpdate.getName());
+		return repo.save(genericClass);
+	}
+	
+	public Map<String, Boolean> delete(Integer id) throws Exception {
+		GenericClass genericClass = repo.findById(id).orElseThrow(() -> new Exception("Crud not found: " + id));
+		repo.delete(genericClass);
+			
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		return response;
+
+	}
+}
